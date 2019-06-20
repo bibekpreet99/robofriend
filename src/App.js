e3edd30720d547729dafd643acd1cfb8a670d200
bibekpreet99 <bibekpreet99@gmail.com>
2019-06-21 01:24:16 +0530

    used a user API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ""
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(user=>this.setState({robots: user}))
+
+    }
+
     onSearchChange = (event)=>{
         return(
             this.setState({
@@ -23,13 +31,24 @@ class App extends Component {
         const filteredArray = this.state.robots.filter((robot)=>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return(
-            <div className="tc">
-                <h1>ROBOFRIEND</h1>
-                <SearchBox change={this.onSearchChange}/>
-                <CardList robots={ filteredArray }/>
-            </div>
+        if(this.state.robots.length === 0){
+                return(
+                    <div className="tc">
+                        <h1 className="f1">ROBOFRIEND</h1>
+                        <SearchBox change={this.onSearchChange}/>
+                        <h1>Loading...</h1>
+                    </div>
+                )
+        }
+        else{ 
+            return(
+                <div className="tc">
+                    <h1 className="f1">ROBOFRIEND</h1>
+                    <SearchBox change={this.onSearchChange}/>
+                    <CardList robots={ filteredArray }/>
+                </div>
         )
+    }
     }
 }
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,6 +3,7 @@ import React from "react"
 const SearchBox = ({ change })=>{
     return(
         <input 
+            className="pa3 ma2 bg-lightest-blue"
             placeholder="search Robots..." 
             type="search"
             onChange = { change }
